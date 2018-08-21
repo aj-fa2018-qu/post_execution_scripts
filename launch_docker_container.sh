392e9db8e304ca8382d75114a053fc98435847e7
Hemal Shah <hs.su2018.qu@gmail.com>
2018-08-21 16:06:16 -0400

    post execution scripts for matlab-nul usecase

---
+++ launch_docker_container.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# This bash program is responsible for launching an docker 
+# container using the values stored in the form of 
+# environment variables. Following variables are used: 
+# DOCKER_IMAGE_NAME 
+# MAC_ADDRESS
+
+source ~/.profile
+
+sudo docker run --name project -td -p 80:8080 --mac-address=$MAC_ADDRESS $DOCKER_IMAGE_NAME
+sudo docker exec project /bin/sh -c 'if [ -f run.sh ]; then (bash run.sh fi)' 
