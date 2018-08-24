5d2b836c13b7f982256b68197bd0458e0c11f4ff
Hemal Shah <hs.su2018.qu@gmail.com>
2018-08-24 14:40:17 -0400

    binding mac address only if it's provided.

--- launch_docker_container.sh
+++ launch_docker_container.sh
@@ -8,5 +8,13 @@
 
 source ~/.profile
 
-sudo docker run --name project -td -p 80:8080 -v /home/ubuntu:$(sudo docker run $DOCKER_IMAGE_NAME pwd)/work --mac-address=$MAC_ADDRESS $DOCKER_IMAGE_NAME
+MAC_BINDING=""
+if [ -z "$MAC_ADDRESS" ];
+then
+	echo 'mac address is not going to be binded.';
+else
+	MAC_BINDING="--mac-address='$MAC_ADDRESS'"
+fi
+
+sudo docker run --name project -td -p 80:8080 -v /home/ubuntu:$(sudo docker run $DOCKER_IMAGE_NAME pwd)/work $MAC_BINDING $DOCKER_IMAGE_NAME
 sudo docker exec project /bin/sh -c 'if [ -f run.sh ]; then (bash run.sh) fi' 
